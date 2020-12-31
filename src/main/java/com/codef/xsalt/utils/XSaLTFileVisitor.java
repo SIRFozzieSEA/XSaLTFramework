31c238eb925d9b0c86bfd6e7ec82edb93f60d11e
Stephan Cossette <github@codef.com>
2020-12-30 16:10:53 -0800

    Refactored file visitor for re-usability

--- src/main/java/com/codef/xsalt/utils/XSaLTFileVisitor.java
+++ src/main/java/com/codef/xsalt/utils/XSaLTFileVisitor.java
@@ -1,112 +1,57 @@
 package com.codef.xsalt.utils;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+import org.apache.log4j.Logger;
 
 /**
  * @author Stephan P. Cossette
  * @author Copyright 2011 Codef.com
  */
-public class XSaLTFileVisitor
-{
+public class XSaLTFileVisitor {
 
-	private String ioTestFilePath;
-	private StringBuffer ioTextBuffer = new StringBuffer();
+	private static final Logger LOGGER = Logger.getLogger(XSaLTFileVisitor.class.getName());
 
-	/*
+	public static int fileCount = 0;
 
-	try
-	{
-		XSaLTFileVisitor oFileVisitor = new XSaLTFileVisitor();
-		File oFile = new File("C:/SVN/TMAClientJava");
-		oFileVisitor.visitAllFiles(oFile);
-		oFileVisitor.writeBufferToFile("C:/_WORKING/TMAClientJava.txt");
-		
-		oFileVisitor = new XSaLTFileVisitor();
-		oFile = new File("C:/SVN/TMAIPBroadcaster");
-		oFileVisitor.visitAllFiles(oFile);
-		oFileVisitor.writeBufferToFile("C:/_WORKING/TMAIPBroadcaster.txt");
-		
-		oFileVisitor = new XSaLTFileVisitor();
-		oFile = new File("C:/SVN/XSaLTWebs");
-		oFileVisitor.visitAllFiles(oFile);
-		oFileVisitor.writeBufferToFile("C:/_WORKING/XSaLTWebs.txt");
-		
-		oFileVisitor = new XSaLTFileVisitor();
-		oFile = new File("C:/SVN/TMABackup");
-		oFileVisitor.visitAllFiles(oFile);
-		oFileVisitor.writeBufferToFile("C:/_WORKING/TMABackup.txt");
-		
-		oFileVisitor = new XSaLTFileVisitor();
-		oFile = new File("C:/SVN/ZZZ_OLD_CODE");
-		oFileVisitor.visitAllFiles(oFile);
-		oFileVisitor.writeBufferToFile("C:/_WORKING/OldCode.txt");
+	public HashMap<String, String> argumentsMap = new HashMap<String, String>();
 
-		
+	public XSaLTFileVisitor(HashMap<String, String> argumentsMap) {
+		if (argumentsMap != null) {
+			this.argumentsMap = argumentsMap;
+		}
 	}
-	catch (IOException e)
-	{
-		
+
+	public HashMap<String, String> getArgumentsMap() {
+		return this.argumentsMap;
 	}
-	
-	*/
 
-	/**
-	 * This method traverses a directory and puts the contents of each file
-	 * into a StringBuffer. 
-	 * 
-	 * @param _oDirectory
-	 *            Directory to traverse as a File object
-	 * @throws IOException
-	 */
-	public void visitAllFiles(File _oDirectory) throws IOException
-	{
-		if (_oDirectory.isDirectory())
-		{
-			String[] aoDirectoryChildren = _oDirectory.list();
-			if (aoDirectoryChildren != null)
-			{
-				for (int i = 0; i < aoDirectoryChildren.length; i++)
-				{
-					ioTestFilePath = _oDirectory.getAbsolutePath() + "\\" + aoDirectoryChildren[i];
-					File oTestDirectory = new File(ioTestFilePath);
-					if (!oTestDirectory.isDirectory())
-					{
-						ioTestFilePath = ioTestFilePath.replace('\\', '/');
-						//						String sTestOne = ioTestFilePath.substring(0,
-						//								ioTestFilePath.lastIndexOf('/') + 1);
-						String sTestTwo = ioTestFilePath.substring(ioTestFilePath.lastIndexOf('/') + 1, ioTestFilePath.length());
+	public void startVisit(String filePath) {
+		visitFiles(Paths.get(filePath));
+	}
 
-						if (sTestTwo.endsWith(".xsl") || sTestTwo.endsWith(".java") || sTestTwo.endsWith(".xml"))
-						{
-
-							StringBuffer oTempBuffer = new StringBuffer();
-							oTempBuffer = XSaLTFileSystemUtils.writeFileToStringBuffer(ioTestFilePath, "\n");
-
-							ioTextBuffer.append(oTempBuffer);
-							ioTextBuffer.append("\n\n BEGIN NEW --------------------------------------------------------------\n\n");
-
-						}
-
-					}
-					visitAllFiles(new File(_oDirectory, aoDirectoryChildren[i]));
+	private void visitFiles(Path path) {
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+			for (Path entry : stream) {
+				if (entry.toFile().isDirectory()) {
+					visitFiles(entry);
+				} else {
+					visitFileCode(entry.toString());
+					fileCount++;
 				}
 			}
+		} catch (IOException e) {
+			LOGGER.error(e.toString(), e);
 		}
-
 	}
 
-	/**
-	 * This method writes the buffer created in the visitAllFiles method to a
-	 * file.
-	 * 
-	 * @param _sFilePath
-	 *            Destination file for buffer
-	 * @throws IOException
-	 */
-	public void writeBufferToFile(String _sFilePath) throws IOException
-	{
-		XSaLTFileSystemUtils.writeStringBufferToFile(ioTextBuffer, _sFilePath);
+	public void visitFileCode(String filePath) {
+		LOGGER.info("File " + filePath + " has been visited.");
 	}
 
 }
