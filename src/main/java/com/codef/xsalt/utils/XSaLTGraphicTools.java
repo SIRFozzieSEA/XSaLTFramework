debe5af165c6a7473070480018dd0a43083c7991
SIRFozzieSEA <github@codef.com>
2021-04-21 09:33:23 -0700

    Added resizeImage() method

--- src/main/java/com/codef/xsalt/utils/XSaLTGraphicTools.java
+++ src/main/java/com/codef/xsalt/utils/XSaLTGraphicTools.java
@@ -141,6 +141,17 @@ public class XSaLTGraphicTools {
 	public XSaLTGraphicTools() {
 
 	}
+	
+	
+	public static BufferedImage resizeImage(BufferedImage originalImage, int targetWidth, int targetHeight) throws IOException {
+	    BufferedImage resizedImage = new BufferedImage(targetWidth, targetHeight, BufferedImage.TYPE_INT_RGB);
+	    Graphics2D graphics2D = resizedImage.createGraphics();
+	    graphics2D.drawImage(originalImage, 0, 0, targetWidth, targetHeight, null);
+	    graphics2D.dispose();
+	    return resizedImage;
+	}
+	
+	
 
 	/**
 	 * This method reads a PDF file and converts the file and writes it into a JPEG
